f452025d1c597c7f2b29b9d4d746499a545418f7
jonggu2020 <75966566+jonggu2020@users.noreply.github.com>
2025-11-27 23:31:52 +0900

    Update py2.py

--- py2.py
+++ py2.py
@@ -12,7 +12,7 @@ SUSPECT_LIST_FILE = "./suspect_list.txt"
 
 # ⚠️ [수정필요 2]
 # 원본 CSV 파일 (v9 스크립트로 생성한 최신 CSV)ㄴ
-# 예: "./master_summary_v11_cleaned_final.csv"ㄴ
+# 예: "./master_summary_v11_cleaned_final.csv"ㄴ4
 ORIGINAL_CSV_FILE = "./master_summary_v11_cleaned_final.csv" 
 
 # ⚠️ [수정필요 3]
@@ -242,5 +242,6 @@ if __name__ == "__main__":
         app = ImageReviewer(root)
 
         root.mainloop()
+
 
 
